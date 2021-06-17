df3c8f2efff66fbdaeecaac5a6a6d15993c082d6
Yootoyo <77391076+Yootoyo@users.noreply.github.com>
2021-06-17 13:11:03 +0800

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,13 @@
+// 这里是数据绑定页面
+var vm = new Vue({
+    el:'#app',
+    data:{
+        motto:'「  充盈、满溢、而后错过  」',
+        name:'「  这是Profile的个人主页  」',
+        introduce:'「  即将实习的在校大学生一枚  」',
+        mail:'邮箱：Yintianyang_like@163.com',
+        qq:'QQ：2110705459',
+        plan:'任务栏透明+居中，win10美化方案',
+        recommend:'Edge浏览器超好用拓展推荐',
+    }
+})
